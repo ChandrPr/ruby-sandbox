0fc941c16a8d38099fdfecf3ef537172b430bd67
ChandrPr <praphulla91@gmail.com>
2024-10-19 15:55:44 +0000

    Pause on Umbrella

--- umbrella.rb
+++ umbrella.rb
@@ -4,8 +4,8 @@ require "json"
 require "awesome_print"
 
 puts "Enter your location!"
-user_input = gets.chomp
-
+#user_input = gets.chomp
+user_input = "chicago"
 puts "Checking the weather at #{user_input}...."
 
 gmap_raw_resp = HTTP.get(
@@ -22,6 +22,11 @@ latitude = gmap_json["geometry"]["location"]["lat"]
 longitude = gmap_json["geometry"]["location"]["lng"]
 puts "Your coordinates are #{latitude}, #{longitude}."
 
+PIRATE_WEATHER_KEY = ENV.fetch("PIRATE_WEATHER_KEY")
+weather_raw_resp = HTTP.get(
+    "https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}",
+)
+weather_json = JSON.parse(weather_raw_resp)
+pp weather_json.keys
 
-
-puts "It is currently #{curtemp}."
+#puts "It is currently #{curtemp}."
