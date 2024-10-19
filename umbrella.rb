a6d814083286f9ca0dd65e47c3947630a17a6a73
ChandrPr <praphulla91@gmail.com>
2024-10-19 15:49:40 +0000

    Got lat long.

--- umbrella.rb
+++ umbrella.rb
@@ -1,2 +1,27 @@
 require "dotenv/load"
-pp ENV.fetch("GMAPS_KEY")
+require "http"
+require "json"
+require "awesome_print"
+
+puts "Enter your location!"
+user_input = gets.chomp
+
+puts "Checking the weather at #{user_input}...."
+
+gmap_raw_resp = HTTP.get(
+    "https://maps.googleapis.com/maps/api/geocode/json",
+    {
+      :params => {
+        "address" => user_input,
+        "key" => ENV.fetch("GMAPS_KEY"),
+      },
+    }
+  )
+gmap_json = JSON.parse(gmap_raw_resp)["results"][0]
+latitude = gmap_json["geometry"]["location"]["lat"]
+longitude = gmap_json["geometry"]["location"]["lng"]
+puts "Your coordinates are #{latitude}, #{longitude}."
+
+
+
+puts "It is currently #{curtemp}."
